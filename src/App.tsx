d9e2ad2341389e316f5d99301b9c118c249f9de6
kkylych <kchannel27@gmail.com>
2025-02-12 19:05:30 +0600

    created navbar

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="w-full h-full absolute">
+      <Navbar />
+      
+    </div>
+  );
+}
+
+export default App;
